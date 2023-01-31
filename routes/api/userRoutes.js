d3c8a3661d4c520814add10e34353d2fe760b8d2
saitek <saitek300@gmail.com>
2023-01-31 15:25:34 -0800

    created friend routes

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -5,10 +5,16 @@ const {
     createUser,
     deleteUser,
     updateUser,
+    newFriend,
+    removeFriend,
 } = require('../../controllers/userController');
 
 router.route('/').get(getUsers).post(createUser);
 
 router.route('/:userId').get(getSingleUser).put(updateUser).delete(deleteUser);
+
+router.route('/:userId/friends').post(newFriend);
+
+router.route('/:userId/friends/:friendId').delete(removeFriend);
 
 module.exports = router;
