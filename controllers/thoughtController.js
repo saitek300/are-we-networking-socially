b4c1dfcaaca0711a9c8202b494ff055390f1e411
saitek <saitek300@gmail.com>
2023-02-25 20:34:36 -0800

    fixed getThoughts and finished createThoughts

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -4,12 +4,13 @@ module.exports = {
     //find all thoughts
     getThoughts(req, res) {
         Thought.find()
-            .then(async (thoughts) => {
+            .sort({ createdAt: -1 })
+            .then((thoughts) => {
                 res.json(thoughts);
             })
             .catch((err) => {
                 console.log(err);
-                return res.status(500), json(err);
+                res.status(500), json(err);
             });
     },
     //find thought by id
@@ -29,7 +30,18 @@ module.exports = {
     //create new thought
     createThought(req, res) {
         Thought.create(req.body)
-            .then((thought) => res.json(thought))
+            .then((thought) =>{
+                return User.findOneAndUpdate(
+                    { _id: req.body.userId },
+                    { $push: { thoughts: thought._id } },
+                    { new: true }
+                  );
+                })
+                .then((thought) =>
+                !thought
+                    ? res.status(404).json({ message: 'thought does not exist' })
+                    : res.json({ thought })
+            )
             .catch((err) => res.status(500).json(err));
     },
     //delete thought by id
