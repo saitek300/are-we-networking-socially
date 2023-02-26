01918e9aeeadb195163108f0c179ec64ed7f3452
saitek <saitek300@gmail.com>
2023-02-25 21:03:07 -0800

    createReation bugfix

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -67,22 +67,24 @@ module.exports = {
         Thought.findOneAndUpdate(
             { _id: req.params.thoughtId },
             { $addToSet: { reactions: req.body } },
-
-        )
-            .then((thought) =>
-                !thought
-                    ? res
-                        .status(404)
-                        .json({ message: "no thought found with this Id" })
-                    : res.json(thought)
-            )
-    },
-
+            { runValidators: true, new: true }
+          )
+            .then((thought) => {
+              !thought
+                ? res.status(404).json({ message: 'No thought with this id!' })
+              
+                : res.json(thought);
+            })
+            .catch((err) => {
+              console.log(err);
+              res.status(500).json(err);
+            });
+        },
     removeReaction(req, res) {
         Thought.findOneAndUpdate(
             { _id: req.params.thoughtId },
             { $pull: { reactions: { _id: req.params.reactionId } } },
-
+            { runValidators: true, new: true }
         )
             .then((thought) =>
                 !thought
